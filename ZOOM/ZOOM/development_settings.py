2182d58305b57f760bcfa16958ad410169c9fbfc
HatimKH <h.khomsi@gmail.com>
2017-01-19 13:17:18 +0100

    #1 Add GeoApp§vocablary & Task_queue & Django_toolbar
    #1 Add GeoApp§vocablary & Task_queue & Django_toolbar

---
+++ ZOOM/ZOOM/development_settings.py
@@ -0,0 +1,50 @@
+# Sample production settings, change as needed
+
+from ZOOM.base_settings import *
+
+DEBUG = True
+
+MIDDLEWARE_CLASSES += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INSTALLED_APPS += {
+    'debug_toolbar',
+}
+
+def custom_show_toolbar(self):
+    return True
+
+SECRET_KEY = '__DEV_SECRET_KEY__'
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'zoom',
+        'USER': 'zoom',
+        'PASSWORD': 'zoom',
+        'HOST': '127.0.0.1',
+    },
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_served/')
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static/'),
+)
+
+FIXTURE_DIRS = (
+     os.path.join(BASE_DIR, '../fixtures/'),
+)
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
---
+++ ZOOM/ZOOM/test_settings.py
@@ -0,0 +1,10 @@
+from ZOOM.production_settings import *
+
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': ':memory:',
+    },
+}
