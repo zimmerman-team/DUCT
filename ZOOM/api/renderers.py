39311b458b6eababf03814d2ff422f750ac4352f
HatimKH <h.khomsi@gmail.com>
2017-02-15 01:57:20 +0000

    Missing file

---
+++ ZOOM/api/renderers.py
@@ -0,0 +1,11 @@
+from rest_framework_csv.renderers import CSVRenderer
+
+
+class PaginatedCSVRenderer (CSVRenderer):
+    results_field = 'results'
+    header = []
+
+    def render(self, data, *args, **kwargs):
+        if not isinstance(data, list):
+            data = data.get(self.results_field, [])
+        return super(PaginatedCSVRenderer, self).render(data, *args, **kwargs)
