130cf10b5a07a998fc9f62769dccf180bfc27ccf
Bryan Haakman <bryanhaakman@gmail.com>
2017-03-06 13:09:00 +0200

    aggregations endpoint

--- ZOOM/api/indicator/urls.py
+++ ZOOM/api/indicator/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
-from api.indicator.views import IndicatorDataList
-# from api.indicator.views import IndicatorAggregations
+from api.indicator.views import IndicatorDataList, IndicatorDataAggregations
 
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$',
         IndicatorDataList.as_view(),
         name='indicator-list'),
-    ]
+    url(r'^aggregations/$',
+        cache_page(settings.API_CACHE_SECONDS)(IndicatorDataAggregations.as_view()),
+        name='indicator-aggregations'),
+]
