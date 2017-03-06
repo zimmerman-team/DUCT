3296ed3edcd09d0ef8f47004163c2795c2764c75
Bryan Haakman <bryanhaakman@gmail.com>
2017-03-06 11:53:05 +0200

    add filters to indicator data list

---
+++ ZOOM/api/indicator/filters.py
@@ -0,0 +1,19 @@
+
+from django_filters import Filter, FilterSet
+from api.generics.filters import CommaSeparatedCharFilter
+from indicator.models import IndicatorDatapoint
+
+class IndicatorDataFilter(FilterSet):
+
+    class Meta:
+        model = IndicatorDatapoint
+        fields = (
+            'id',
+            'file_source_id',
+            'date_format_id',
+            'indicator_category_id',
+            'indicator_id',
+            'country_id',
+            'date_value',
+            'source_id',
+        )
