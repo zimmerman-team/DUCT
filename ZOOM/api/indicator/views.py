94747cf9afa6f410c6c48c461d2ae87cf8f91fd2
Bryan Haakman <bryanhaakman@gmail.com>
2017-03-06 13:09:00 +0200

    aggregations endpoint

--- ZOOM/api/indicator/views.py
+++ ZOOM/api/indicator/views.py
@@ -8,6 +8,9 @@ from indicator.models import IndicatorDatapoint
 
 from api.indicator.filters import IndicatorDataFilter
 
+from api.aggregation.views import AggregationView, Aggregation, GroupBy
+
+from django.db.models import Count, Sum, F
 
 class IndicatorDataList(DynamicListView):
 
@@ -28,3 +31,62 @@ class IndicatorDataList(DynamicListView):
         'measure_value',
         'other',
         )
+
+
+class IndicatorDataAggregations(AggregationView):
+    """
+    Returns aggregations based on the item grouped by, and the selected aggregation.
+
+    ## Group by options
+
+    API request has to include `group_by` parameter.
+    
+    This parameter controls result aggregations and
+    can be one or more (comma separated values) of:
+
+    - thingiemahbob
+
+    ## Aggregation options
+
+    API request has to include `aggregations` parameter.
+    
+    This parameter controls result aggregations and
+    can be one or more (comma separated values) of:
+
+    - `count`
+    - `count_distinct`
+
+    ## Request parameters
+
+    All filters available on the Activity List, can be used on aggregations.
+
+    """
+
+    queryset = IndicatorDatapoint.objects.all()
+
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = IndicatorDataFilter
+
+    allowed_aggregations = (
+        Aggregation(
+            query_param='count',
+            field='count',
+            annotate=Count('id'),
+        ),
+        Aggregation(
+            query_param='count_distinct',
+            field='count',
+            annotate=Count('id', distinct=True),
+        ),
+    )
+
+    allowed_groupings = (
+        GroupBy(
+            query_param="indicator_category_id",
+            fields="indicator_category_id",
+        ),
+        GroupBy(
+            query_param="indicator_id",
+            fields="indicator_id",
+        ),
+    )
