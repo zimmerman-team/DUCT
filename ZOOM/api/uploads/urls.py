d5a6625a408653176f993592697da74c68f86d97
HatimKH <h.khomsi@gmail.com>
2017-03-22 14:28:10 +0000

    update views

--- ZOOM/api/uploads/urls.py
+++ ZOOM/api/uploads/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url, include
-from api.uploads.views import FilesCreateList
+from django.conf.urls import url
+from api.uploads.views import UploadsCreateList
 # from rest_framework import routers
 
 # router = routers.DefaultRouter()
 # router.register('files', FilesCreateList, 'files')
 urlpatterns = [
-        # url(r'^$', FilesCreateList.as_view(), name='files-list'),
+        url(r'^$', UploadsCreateList.as_view(), name='uploads-list'),
         # url(r'^$', include(router.urls)),
 ]
 
