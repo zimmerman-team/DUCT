dd9814c12f2983d7bb0ce8380a16857131a58cd5
kjod <kieranjamesodriscoll@gmail.com>
2017-03-30 14:10:45 +0100

    Fixed Django frontend to offer basic mapping function for a user

--- ZOOM/api/urls.py
+++ ZOOM/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^uploads/', include('api.uploads.urls', namespace='uploads')),
     url(r'^scatter/', include('api.scatter.urls', namespace='scatter')),
     url(r'^file/', include('api.file.urls')),
+    url(r'^validate/', include('api.validate.urls')),
     ]
 
