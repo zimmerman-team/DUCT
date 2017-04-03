3bc1c64e56bc1c01370ea8090a2e4e47664754d0
HatimKH <h.khomsi@gmail.com>
2017-04-03 18:18:59 +0100

    add errors serializers

--- ZOOM/api/uploads/urls.py
+++ ZOOM/api/uploads/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from api.uploads.views import UploadsCreateList
+from api.uploads.views import UploadsCreateList, MapperView
 
 
 urlpatterns = [
         url(r'^$', UploadsCreateList.as_view(), name='uploads-list'),
+        url(r'^mapper/$', MapperView.as_view(), name='mapper-list'),
 ]
 
