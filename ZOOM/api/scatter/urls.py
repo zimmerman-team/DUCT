5557b753a2ead79561351dea2ba2b163d51839eb
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2017-04-11 12:39:43 +0200

    removing unused scatter endpoints

--- ZOOM/api/scatter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from api.scatter.views import ScatterDataList
-
-
-urlpatterns = [
-    url(r'^$',
-        ScatterDataList.as_view(),
-        name='scatter-list'),
-    ]
