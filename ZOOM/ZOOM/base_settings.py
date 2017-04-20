f58e9cf8059e3d6c06b6892033d36006c80b8a63
HatimKH <h.khomsi@gmail.com>
2017-04-20 17:17:41 +0100

    update settings file

--- ZOOM/ZOOM/base_settings.py
+++ ZOOM/ZOOM/base_settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
-        'api.renderers.PaginatedCSVRenderer',
+        # 'api.renderers.PaginatedCSVRenderer',
     ),
 }
 
