2814eee17a63a9f954d60760b1c8cf703a03cea4
HatimKH <h.khomsi@gmail.com>
2017-04-20 12:15:56 +0100

    Normalize validate page

---
+++ ZOOM/api/validate/validator.py
@@ -0,0 +1,131 @@
+# from django.http import Http404
+# from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.template import loader
+# from django.http import HttpResponse
+# from django.shortcuts import render_to_response
+# from django.template import RequestContext
+# from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
+# from django.core.cache import cache
+from django.conf import settings
+# from django.core import serializers
+# from django.contrib.staticfiles.templatetags.staticfiles import static
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# import json
+import os
+import pickle
+import uuid
+import numpy as np
+import pandas as pd
+
+from indicator.models import IndicatorDatapoint
+# from lib.converters import convert_spreadsheet
+from lib.tools import identify_col_dtype
+from validate.models import File
+from geodata.importer.country import CountryImport
+from geodata.models import get_dictionaries
+
+
+def validate(file_id):
+    # file_id = file_id
+    #tmp
+    newdoc = ['Item'] 
+    validate_form = True
+    count = 0
+    overall_count = 0
+
+    #change name -> doesn't make sense
+    missing_mapping = [] 
+    #also doesn't make sense any more 
+    remaining_mapping = []
+    missing_datatypes = []
+    found_mapping = []
+    # what does this mean????
+    mapping = [] 
+    validation_results = []
+    dtypes_list = []
+    error_lines = []
+    summary_results = []
+    summary_indexes = []
+
+    dtypes_dict = {}
+    headings_template = {}
+    headings_file = {}
+
+    count = 0
+    newdoc[0] = str(File.objects.get(id=file_id).file)
+
+    #loop here for multiple files
+
+    #.get_file_path())
+    df_file = pd.read_csv(newdoc[0])
+    file_heading_list = df_file.columns
+
+    # might be too big, might take too long
+    #sample_amount = len(df_file[file_heading_list[0]]) 
+
+    template_heading_list = []
+    
+    #Get datapoint headings
+    for field in IndicatorDatapoint._meta.fields:
+        template_heading_list.append(field.name)#.get_attname_column())
+    
+    #skip first four headings as irrelevant to user input
+    template_heading_list = template_heading_list[4:len(template_heading_list)]
+
+    #template_heading_list.append("unit_measure") #needed? 
+    
+    #get dicts for country
+    dicts, _ = get_dictionaries()
+    for heading in file_heading_list:
+        headings_file[heading] = count
+        prob_list, error_count = identify_col_dtype(df_file[heading], heading, dicts)
+        dtypes_dict[heading] = prob_list 
+        
+        error_lines.append(error_count)
+        validation_results.append(df_file[heading].isnull().sum())
+        dtypes_list.append(prob_list)
+        column_detail = df_file[heading].describe()
+        summary_results.append(np.array(column_detail).astype('str'))
+        summary_indexes.append(list(column_detail.index))
+
+
+    #count matching
+    count = 0 
+    overall_count = len(template_heading_list)
+
+    for key in template_heading_list:
+      remaining_mapping.append(key)
+    
+    files = []
+    files_id = []
+    files.append(newdoc[0])
+    #summary_results won't zip correctly
+    zip_list = zip(file_heading_list, dtypes_list, validation_results)
+    missing_mapping = list(headings_file.keys())
+    
+    path = os.path.join(os.path.dirname(settings.BASE_DIR), 'ZOOM/media/tmpfiles')
+    dict_name = path +  "/" + str(uuid.uuid4()) + ".txt"
+    with open(dict_name, 'w') as f:
+        pickle.dump(dtypes_dict, f)  
+    
+    context = {
+        'success': 1, 
+        'mapped' : count, 
+        "no_mapped" : overall_count - count, 
+        "found_list": zip_list, 
+        "summary":zip(summary_indexes, summary_results), 
+        "missing_list" : remaining_mapping, 
+        "files" : files[0], 
+        "dtypes_loc" : dict_name
+    }
+
+    # print(len(summary_results));
+    # print(len(validation_results));
+
+    #output need to pass allignments of mapped headings
+    return context
