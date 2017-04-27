7a626109c2fdb6597ca93ece20c9f5b5f70bb471
HatimKH <h.khomsi@gmail.com>
2017-04-27 16:04:24 +0100

    merge conflict resolution urls

--- ZOOM/api/file/urls.py
+++ ZOOM/api/file/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import url
 
-from api.file.views import FileListView, FileDetailView, FileSourceListView, FileTagListView, FileView
+from api.file.views import FileListView, FileDetailView, FileSourceListView, FileTagListView
 
 
 urlpatterns = [
-<<<<<<< HEAD
-        url(r'^$', FileView.as_view(), name='file'),
-        url(r'^file-list/$', FileListView.as_view(), name='file-list'),
-        url(r'^file-list/(?P<pk>[0-9a-f-]+)/$', FileDetailView.as_view(), name='file-detail'),
-=======
         url(r'^$', FileListView.as_view(), name='file-list'),
         url(r'^(?P<pk>[^@$&+,/:;=?]+)/', FileDetailView.as_view(), name='file-detail'),
->>>>>>> upstream/develop
         url(r'^sources/', FileSourceListView.as_view(), name='file-source-list'),
-        url(r'^tags/$', FileTagListView.as_view(), name='file-tag-list')
+        url(r'^(?P<pk>\d+)/tags/$', FileTagListView.as_view(), name='file-tag-list')
 ]
 
 
