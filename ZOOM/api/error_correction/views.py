f8774eb3d3519e144c73c610499a7053d437271a
kjod <kieranjamesodriscoll@gmail.com>
2017-05-08 15:47:28 +0100

    Added complete functionality for error correction api

--- ZOOM/api/error_correction/views.py
+++ ZOOM/api/error_correction/views.py
@@ -2,10 +2,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
-from error_correction.error_correction import error_correction
+from error_correction.error_correction import *
 
 
 @api_view(['POST'])
 def ErrorCorrectionView(request):
-    context = error_correction(request)
+    if 'save' in request.data:#split into views
+    	context = update(request)
+    elif 'get_errors' in request.data:
+    	context = get_errors(request)
+    else:	
+    	context = error_correction(request)
     return Response(context)
--- ZOOM/error_correction/error_correction.py
+++ ZOOM/error_correction/error_correction.py
@@ -8,6 +8,68 @@ import numpy as np
 import pandas as pd
 
 
+def get_errors(request):
+    print("getting errors")
+    file_id = request.data['file_id']
+    dtypes_dict = request.data['dtypes_dict']
+    start_pos = request.data['start_pos']
+    end_pos = request.data['end_pos']
+
+    """  remove this dependency, store headings in dict for error data"""
+    file_name = File.objects.get(id=file_id).file
+    df_data = pd.read_csv(file_name)
+    column_headings = df_data.columns
+    """                         """
+
+    dtypes_dict_file_name = FileDtypes.objects.get(file=File.objects.get(id=file_id)).dtype_name 
+    
+    with open(str(dtypes_dict_file_name), 'rb') as f:
+        error_data = pickle.load(f)
+    
+    errors = {}
+    counter = 0
+
+    for i in column_headings:#minus one for line no
+        #g = lambda x: if x[0] == 
+        indexes = [x for x in error_data[counter][start_pos:end_pos] if x[0] != dtypes_dict[i][0][0]] #filter(, error_data[i][start_pos:end_pos])#np.where(data_types != dtypes_dict[column_headings[i]][0][0])
+        print("required ", dtypes_dict[i][0][0], " what we have ", error_data[counter][start_pos:end_pos])
+        print(indexes)
+        errors[i] =  indexes
+        counter += 1
+    print(errors)
+
+    temp_error_message = {}
+    for i in errors:
+        for j in errors[i]:#minus one for line no
+            message = ("Found " + j[0] + ", should be " + dtypes_dict[i][0][0])
+            temp_error_message[''.join([str(j[1]),"|",i])] = (message)
+
+    context = {"error_messages": temp_error_message}
+    print(context)
+
+    return context
+
+def update(request):
+    if request.data['type'] == "csv":
+        print("Request ", request.data)
+        file_id = request.data['file_id']
+        print("File location ", File.objects.get(id=file_id).file)
+        df_data = pd.read_csv(File.objects.get(id=file_id).file)
+        row_data = request.data['row']
+        row_data.pop('index', None)
+        line_no = row_data.pop('line no.')
+
+
+        for i in row_data:
+            print('heading: ', i)
+            print("replacing ", df_data[i][line_no], "  ", row_data[i])
+            df_data[i][line_no] = row_data[i]
+        
+        with open(str(File.objects.get(id=file_id).file), 'w') as f:
+            df_data.to_csv(f)
+
+    return {"success" : 1}
+
 def error_correction(request):
     print("----------------------checking data-------------------------")
     print(request.data)
@@ -27,8 +89,10 @@ def error_correction(request):
     if request.data["filter_toggle"]:
         #look for empty string if given or nan's
         if request.data['replace_pressed']:
-            df_data[df_data[request.data["filter_value"]] == request.data['find_value']] = request.data['replace_value']
-            df_data.to_csv(File.objects.get(id=file_id).file)#save update file
+            print("this is the replace value: ",request.data['replace_value'])
+            df_data[request.data["filter_value"]][df_data[request.data["filter_value"]] == request.data['find_value']] = request.data['replace_value']
+            with open(str(File.objects.get(id=file_id).file), 'w') as f:
+                df_data.to_csv(f)
             df_data = df_data[df_data[request.data["filter_value"]] == request.data['replace_value']]
         else:
             df_data = df_data[df_data[request.data["filter_value"]] == request.data['find_value']]
@@ -42,11 +106,12 @@ def error_correction(request):
     org_data['line_no'] = org_data.index.values
     org_data = org_data.reset_index()
     df_data = df_data.reset_index()
-    print(df_data)
+    #print(df_data)
     counter = 0
-    for start_pos in range(end_pos):
-        print("counter ", counter ," length of dataframe ", len(str(df_data[df_data.columns[0]])))
-        if counter >= len(df_data[df_data.columns[0]]):
+    print("start pos ", start_pos, " end pos ", end_pos, "length ", len(df_data[df_data.columns[0]]) - 1)
+    start = start_pos
+    for start_pos in range(start, end_pos):
+        if start_pos >= len(df_data[df_data.columns[0]]) - 1:
             break
         temp_dict={}
         temp_dict = {"line no.": org_data['line_no'][start_pos]}
@@ -55,9 +120,11 @@ def error_correction(request):
             #print(df_data.index.get_loc(df_data[column].iloc[start_pos].name))
         output_list.append(temp_dict)
         counter = counter + 1
+        print("check " ,counter, ' start_pos ', start_pos )
 
+    print("counter ", counter ," length of dataframe ", len(str(df_data[df_data.columns[0]])))
     #context = {"table": output_list, "errors": [], "error_message": [], "missing_value": []}
-    context = {"errors" : [], "error_messages" : [], "missing_values" : [], "data_table": json.dumps(output_list), "total_amount": len(df_data[df_data.columns[0]]) }#added json dumps, front end couldn't read original format
+    context = {"data_table": json.dumps(output_list), "total_amount": len(df_data[df_data.columns[0]]) }#added json dumps, front end couldn't read original format
     print("CONTEXT, ", context)
     return context
     #start lines and end lines
