ec0805c48d17029e6e7707d0e8aff599e7d5e8dd
kjod <kieranjamesodriscoll@gmail.com>
2017-05-09 08:47:00 +0100

    Fixed errors in Error correction

--- ZOOM/error_correction/error_correction.py
+++ ZOOM/error_correction/error_correction.py
@@ -66,7 +66,7 @@ def update(request):
             df_data[i][line_no] = row_data[i]
         
         with open(str(File.objects.get(id=file_id).file), 'w') as f:
-            df_data.to_csv(f)
+            df_data.to_csv(f, index=False)
 
     return {"success" : 1}
 
@@ -92,7 +92,7 @@ def error_correction(request):
             print("this is the replace value: ",request.data['replace_value'])
             df_data[request.data["filter_value"]][df_data[request.data["filter_value"]] == request.data['find_value']] = request.data['replace_value']
             with open(str(File.objects.get(id=file_id).file), 'w') as f:
-                df_data.to_csv(f)
+                df_data.to_csv(f, index=False)
             df_data = df_data[df_data[request.data["filter_value"]] == request.data['replace_value']]
         else:
             df_data = df_data[df_data[request.data["filter_value"]] == request.data['find_value']]
@@ -111,7 +111,7 @@ def error_correction(request):
     print("start pos ", start_pos, " end pos ", end_pos, "length ", len(df_data[df_data.columns[0]]) - 1)
     start = start_pos
     for start_pos in range(start, end_pos):
-        if start_pos >= len(df_data[df_data.columns[0]]) - 1:
+        if start_pos > len(df_data[df_data.columns[0]]) - 1:
             break
         temp_dict={}
         temp_dict = {"line no.": org_data['line_no'][start_pos]}
@@ -127,68 +127,3 @@ def error_correction(request):
     context = {"data_table": json.dumps(output_list), "total_amount": len(df_data[df_data.columns[0]]) }#added json dumps, front end couldn't read original format
     print("CONTEXT, ", context)
     return context
-    #start lines and end lines
-    # newdoc_path = request.session['files'][0]
-    #newdoc_name = request.session['files'][0].split("/")[-1]
-    """column_headings = list(df_data.columns)
-    dtypes_dict = request.data['dtypes_dict']
-    dtypes_dict_file_name = FileDtypes.objects.get(file=File.objects.get(id=file_id)).dtype_name 
-    print(dtypes_dict_file_name)
-    with open(dtypes_dict_file_name, 'rb') as f:
-        error_data = pickle.load(f)
-    #error_data = request.data['missing_dtypes_list']
-    #error_data = #he data each line has
-    line_no = np.array([])
-    pointer = []
-    for i in range(len(column_headings)):#minus one for line no
-        data_types = np.array([j[0] for j in error_data[i]]) # column
-        indexes = np.where(data_types != dtypes_dict[column_headings[i]][0][0])
-        line_no = np.append(line_no, indexes)#line_no + list(np.where(data_types == dtypes_dict[column_headings[i]][0][0]))
-        #pointer.append(0)
-    #line_no = np.array(line_no)
-    error_line_no = np.unique(line_no) # get only line numbers that have errors # mioght need to sort array
-
-
-    ##go through headings get the most common dtypes 
-    #search for the lines that have the least common
-    #highlight them some how  using javascript
-    #define ids for html elements by combining column name with id number 'generate this list accordingly'
-
-    #might be better to generate teh html beforehand
-
-    #list according to ids?????
-
-
-    #get dictionary of line number that have error and give columns of error
-    #for i in range(len(df_data[column_headings[0]])):
-    
-    #if len(error_line_no) <= 200: 
-
-    for i in error_line_no:
-        i = int(i)#i float 64
-        temp_list = []
-        temp_error_ids = []
-        for j in range(len(column_headings)):#minus one for line no
-            if dtypes_dict[column_headings[j]][0][0] != error_data[j][i][0]: #error[0][0][ => column 0 line 0 value= (dtype, line_no)#this doesn't make sense
-                #log line to highlight
-                found_error_ids.append((str(i+1)+"_"+column_headings[j], "found " + error_data[j][i][0] + " should be " + dtypes_dict[column_headings[j]][0][0]))
-                #found_error = True
-            temp_list.append(df_data.iloc[i, j])
-            temp_error_ids.append(str(i+1)+"_"+ column_headings[j])
-        #if found_error:
-        line_numbers.append(i + 1)
-        data_list.append(zip(temp_list, temp_error_ids))
-        #error_ids.append(temp_error_ids)
-        #found_error = False
-
-    #zip_list = zip(*[data_list, error_ids])
-    zip_list = zip(line_numbers, data_list)
-    context = {"df_data" : zip_list, "column_headings":column_headings, "found_error_ids" : found_error_ids}
-    
-    #else:
-    #    error_count = len(error_line_no)
-    #    list_of_errors = [] 
-    #    context = {"error_count": len(error_line_no), "list of errors": list_of_errors, "newdoc_name": newdoc_name}"""
-
-    return context
-    #return render(request, 'error_correct/error_correct.html', context)
