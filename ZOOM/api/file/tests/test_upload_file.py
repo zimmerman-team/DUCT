c41760054c689b180a8c9572e88fdc75528231b4
HatimKH <h.khomsi@gmail.com>
2017-06-14 08:15:40 +0100

    Unit tests for CSV uploads

---
+++ ZOOM/api/file/tests/test_upload_file.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from unittest import skip
+from django.test import RequestFactory, Client
+from rest_framework.test import APIClient
+
+
+class FileSaveTestCase(TestCase):
+    request_dummy = RequestFactory().get('/')
+    c = APIClient()
+
+    def test_post_file(self):
+
+        with open('samples/successful_upload_test.csv') as fp:
+                res = self.c.post(
+                        '/api/file/?format=json', 
+                        {
+                        'file': fp,
+                        'title': 'temp title', 
+                        'description': 'temp description', 
+                        'file_name': 'successful_upload_test',
+                        })
+
+        self.assertEquals(res.status_code, 201, res.json())
+        self.assertIsNotNone(res.json()['id'])
