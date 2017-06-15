b70e44c5ca5abc0316e2af10803508c269e772a0
HatimKH <h.khomsi@gmail.com>
2017-06-15 16:16:59 +0100

    Check validate api call

--- ZOOM/api/validate/tests/test_check_file_valid.py
+++ ZOOM/api/validate/tests/test_check_file_valid.py
@@ -10,6 +10,10 @@ class FileValidTestCase(TestCase):
 
     def test_check_file_valid(self):
 
+        '''
+        Test 1: Upload File
+        '''
+
         with open('samples/check_file_valid_test_success.csv') as fp:
                 res_file_upload = self.c.post(
                         '/api/file/?format=json', 
@@ -23,6 +27,10 @@ class FileValidTestCase(TestCase):
         self.assertEquals(res_file_upload.status_code, 201, res_file_upload.json())
         self.assertIsNotNone(res_file_upload.json()['id'])
 
+        '''
+        Test 2: Check File is Valid
+        '''
+
         res_check_file_valid = self.c.post(
             '/api/validate/check_file_valid/?format=json', 
             {
@@ -33,4 +41,24 @@ class FileValidTestCase(TestCase):
 
         self.assertEquals(res_check_file_valid.status_code, 200, res_check_file_valid.json())
         self.assertEquals(res_check_file_valid.json()['success'], 1)
+
+        '''
+        Test 3: Check File Validation results
+        '''
+
+        res_file_validate = self.c.post(
+            '/api/validate/?format=json',
+            {
+            'file_id': res_file_upload.json()['id'],
+            }
+            )
+        
+        # print res_file_validate.json()['found_list']
+
+        self.assertEquals(res_file_validate.status_code, 200, res_file_validate.json())
+        self.assertIsNotNone(res_file_validate.json()['found_list'])
+        self.assertIsNotNone(res_file_validate.json()['missing_list'])
+        self.assertIsNotNone(res_file_validate.json()['summary'])
+        
+        
 
