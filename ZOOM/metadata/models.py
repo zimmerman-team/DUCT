eee7cad954f0682998e03aeb8e22a6e41e93f637
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-10-31 15:50:51 +0100

    Convert metatdata model to PEP 8

--- ZOOM/metadata/models.py
+++ ZOOM/metadata/models.py
@@ -4,12 +4,23 @@ from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 from geodata.models import Geolocation
 
+
 def upload_to(instance, filename='test'):
-    return os.path.join(os.path.join(settings.MEDIA_ROOT, settings.DATASETS_URL), filename)
+    return os.path.join(
+        os.path.join(
+            settings.MEDIA_ROOT,
+            settings.DATASETS_URL),
+        filename)
+
 
 class FileSource(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
-    name = models.CharField(unique=True ,max_length=100, null=False, blank=True)
+    name = models.CharField(
+        unique=True,
+        max_length=100,
+        null=False,
+        blank=True)
+
 
 class File(models.Model):
     ## Metadata related fileds ##
@@ -23,23 +34,34 @@ class File(models.Model):
     methodology = models.CharField(max_length=150)
     define_methodology = models.TextField()
     update_frequency = models.CharField(max_length=100)
-    comments = models.TextField() # caveats
-    accessibility = models.CharField(max_length=100, choices = (('o','open'), ('p','private'), ('r','request')))
+    comments = models.TextField()  # caveats
+    accessibility = models.CharField(max_length=100, choices=(
+        ('o', 'open'), ('p', 'private'), ('r', 'request')))
     data_quality = models.CharField(max_length=1000)
-    number_of_rows = models.IntegerField(help_text='No. of rows within dataset')
-    number_of_rows_saved = models.IntegerField(null=True, help_text='No. of rows from dataset saved within ZOOM')
-    file_types = models.CharField(max_length=100, choices = (('csv','csv'), ('json', 'json')))
+    number_of_rows = models.IntegerField(
+        help_text='No. of rows within dataset')
+    number_of_rows_saved = models.IntegerField(
+        null=True, help_text='No. of rows from dataset saved within ZOOM')
+    file_types = models.CharField(
+        max_length=100, choices=(
+            ('csv', 'csv'), ('json', 'json')))
     data_uploaded = models.DateField(auto_now_add=True)
     last_updated = models.DateField(auto_now=True)
 
-    location = models.ForeignKey(Geolocation, on_delete=models.SET_NULL, null=True)
+    location = models.ForeignKey(
+        Geolocation,
+        on_delete=models.SET_NULL,
+        null=True)
     source = models.ForeignKey(FileSource, on_delete=models.CASCADE)
 
     ## Back-end operational fields ##
     original_file_location = models.CharField(max_length=300)
-    mapping_used = JSONField(null=True) # thge Mapping used for the file
-    file_status = models.CharField(max_length=100, choices=(('1','Uploaded'), ('2','Error Correction'), ('3','Mapping'), ('4','Saved')))
-    datatypes_overview_file_location = models.CharField(max_length=500)#location
+    mapping_used = JSONField(null=True)  # thge Mapping used for the file
+    file_status = models.CharField(max_length=100, choices=(
+        ('1', 'Uploaded'), ('2', 'Error Correction'), ('3', 'Mapping'),
+        ('4', 'Saved')))
+    datatypes_overview_file_location = models.CharField(
+        max_length=500)  # location
     error_file_location = models.CharField(max_length=500)  # location
     file = models.FileField(upload_to=upload_to)
 
@@ -53,7 +75,11 @@ class File(models.Model):
 
     def get_file_path(self):
         print('check56')
-        return os.path.join(settings.MEDIA_ROOT, settings.DATASETS_URL, os.path.basename(self.file.name))
+        return os.path.join(
+            settings.MEDIA_ROOT,
+            settings.DATASETS_URL,
+            os.path.basename(
+                self.file.name))
 
 
 def check_files():
@@ -72,7 +98,7 @@ def check_files():
                 found_tmpfiles.append(str(FileDtypes.objects.get(file=file_ob).dtype_name))
             except Exception:#make more specfic
                 print(file_ob.title, ', Error, can't find dtype_name')
-            
+
             try:
                 found_tmpfiles.append(str(FileDtypes.objects.get(file=file_ob).dtype_dict_name))
             except Exception:
@@ -93,21 +119,22 @@ def check_files():
     datasets_dir = (os.listdir(datasets_path))
     datasets_dir = set([str(datasets_path + '/' + s) for s in datasets_dir])
     tmp_dir = set([str(tmp_path + '/' + s) for s in tmp_dir])
-    
+
     diff_files, diff_tmpfiles = datasets_dir.difference(found_files), tmp_dir.difference(found_tmpfiles)
     file_sets = [diff_files, diff_tmpfiles]
-    
+
     print('size datasets', len(datasets_dir))
     print('size diff', len(diff_files))
     count = 0
-    
+
     for i in range(len(file_sets)):
         if len(file_sets[i]) > 0:
             for j in file_sets[i]:
                 os.remove(j)
                 count = count + 1
-                
+
     print('Removed files: ', count)'''
+
 
 def remove_unmapped_files():
     print('TODO')
@@ -117,7 +144,7 @@ def remove_unmapped_files():
     file_objects = File.objects.all()
     for file_ob in file_objects:
         if file_ob.status < 5:
-            
+
             try:
                 os.remove(str(file_ob.file))
             except Exception: #shuld,'t happen'
@@ -128,7 +155,7 @@ def remove_unmapped_files():
                     os.remove(str(FileDtypes.objects.get(file=file_ob).dtype_name))
                 except Exception:#make more specfic
                     print(file_ob.title, ', Error, can't find dtype_name')
-                
+
                 try:
                     os.remove(str(FileDtypes.objects.get(file=file_ob).dtype_dict_name))
                 except Exception:
@@ -137,5 +164,5 @@ def remove_unmapped_files():
 
             file_ob.delete()
             count += 1
-                
+
     print('Removed: ', count)'''
