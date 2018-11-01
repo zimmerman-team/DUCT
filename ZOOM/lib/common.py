f29a7cb2cebf30e439431c5e7b8fcd77be635811
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-11-01 12:46:08 +0100

    Formated PEP 8

--- ZOOM/lib/common.py
+++ ZOOM/lib/common.py
@@ -13,38 +13,48 @@ import time
 
 def get_geolocation_dictionary():
     dict = {}
-    dict['iso2'] = list(Geolocation.objects.values_list('iso2', flat=True).distinct())
+    dict['iso2'] = list(
+        Geolocation.objects.values_list(
+            'iso2', flat=True).distinct())
     dict['iso2'] = [x for x in dict['iso2'] if x is not None]
     value_dict = {k.lower(): 'iso2' for k in dict['iso2']}
-    dict['iso3'] = list(Geolocation.objects.values_list('iso3', flat=True).distinct())
+    dict['iso3'] = list(
+        Geolocation.objects.values_list(
+            'iso3', flat=True).distinct())
     dict['iso3'] = [x for x in dict['iso3'] if x is not None]
     value_dict = {**value_dict, **{k.lower(): 'iso3' for k in dict['iso3']}}
     for entry in list(Geolocation.objects.distinct('type')):
-        dict[entry.type] = list(Geolocation.objects.filter(type=entry.type).values_list('tag', flat=True).distinct())
+        dict[entry.type] = list(Geolocation.objects.filter(
+            type=entry.type).values_list('tag', flat=True).distinct())
         dict[entry.type] = [x for x in dict[entry.type] if x is not None]
-        value_dict = {**value_dict, **{k.lower(): entry.type for k in dict[entry.type]}}
+        value_dict = {**value_dict,
+                      **{k.lower(): entry.type for k in dict[entry.type]}}
     return value_dict
+
 
 def save_validation_data(error_data, id, dtypes_dict):
     """Saves error data for file.
-    
+
     Args:
         error_data ({str:[str]}): error data for each column..
         id (str): ID of file being used.
         dtypes_dict ({str:str}): stores the data-types for each heading.
     """
 
-    path = os.path.join(os.path.dirname(settings.BASE_DIR), 'ZOOM/media/tmpfiles')
+    path = os.path.join(
+        os.path.dirname(
+            settings.BASE_DIR),
+        'ZOOM/media/tmpfiles')
     tmp_id = str(uuid.uuid4())
-    error_file = path +  "/" + tmp_id + "_error_data.txt"
+    error_file = path + "/" + tmp_id + "_error_data.txt"
 
     with open(error_file, 'wb') as f:
         pickle.dump(error_data, f)
 
-    dtypes_overview = path +  "/" + tmp_id + "_dtype_overview.txt"
+    dtypes_overview = path + "/" + tmp_id + "_dtype_overview.txt"
     with open(dtypes_overview, 'wb') as f:
         pickle.dump(dtypes_dict, f)
-    
+
     instance = File.objects.get(id=id)
 
     if instance.datatypes_overview_file_location:
@@ -57,17 +67,18 @@ def save_validation_data(error_data, id, dtypes_dict):
 
 def get_dtype_data(id):
     """Get data type data for file.
-    
+
     Args:
         id (str): ID of file.
 
-    Returns: 
-        error_data ([[int]]): error data, first list is a column ,second is the row.
+    Returns:
+        error_data ([[int]]): error data, first list is a column
+        ,second is the row.
         dtypes_dict ({str:str}): stores the data-types found for each heading.
     """
 
     file = File.objects.get(id=id)
-    
+
     with open(str(file.datatypes_overview_file_location), 'rb') as f:
         error_data = pickle.load(f)
 
@@ -90,6 +101,7 @@ def save_mapping(id, mapping):
     file.mapping_used = json.dumps(mapping)
     file.save()
 
+
 def get_mapping(id):
     """Get user mapping for file"""
     file = File.objects.get(id=id)
@@ -97,14 +109,17 @@ def get_mapping(id):
         return {'success': 1, 'mapping': json.loads(file.mapping_used)}
     return {'success': 0, 'mapping': None}
 
+
 def get_headings_data_model(df_file):
     """Get column headings and data model headings.
-    
+
     Args:
         df_file (Dataframe): data of csv file in a dataframe.
 
-    Returns: 
-        zip_list: ([str, str, int]), contains file headings, the rest is information not needed but kept due to pre-existing dependency on given data structure.
+    Returns:
+        zip_list: ([str, str, int]), contains file headings,
+        the rest is information not needed but kept due
+        to pre-existing dependency on given data structure.
         summary_indexes ([str]): summary headings for data.
     """
 
@@ -114,34 +129,37 @@ def get_headings_data_model(df_file):
     validation_results = file_heading_list
     data_model_headings = []
 
-    #Get datapoint headings
+    # Get datapoint headings
     for field in Datapoints._meta.fields:
-        data_model_headings.append(field.name)#.get_attname_column())
-    #skip first four headings as irrelevant to user input, should use filter for this
+        data_model_headings.append(field.name)  # .get_attname_column())
+    # skip first four headings as irrelevant to user input, should use filter
+    # for this
     print((time.strftime("%H:%M:%S")))
-    
+
     data_model_headings = data_model_headings[4:len(data_model_headings)]
     data_model_headings = filter(lambda x: "search_vector_text" != x and
-                                            "date_format" != x and
-                                            "date_created" != x and
-                                            "file" != x, data_model_headings)
-    remaining_mapping = data_model_headings    
+                                 "date_format" != x and
+                                 "date_created" != x and
+                                 "file" != x, data_model_headings)
+    remaining_mapping = data_model_headings
     zip_list = zip(file_heading_list, dtypes_list, validation_results)
-    summary_results = file_heading_list##change this to add hover for file heading
-    summary_indexes = file_heading_list##change this 
+    # change this to add hover for file heading
+    summary_results = file_heading_list
+    summary_indexes = file_heading_list  # change this
     print((time.strftime("%H:%M:%S")))
     return zip_list, summary_results, summary_indexes, remaining_mapping
 
 
 def get_column_information(df_file, dtypes_dict):
     """Get information about columns.
-    
+
     Args:
         df_file (Dataframe): data of csv file in a dataframe.
         dtypes_dict ({str:str}): stores the data-types for each heading.
 
-    Returns: 
-        zip_list: ([str, str, int]), contains file heading, list of dtypes for heading, amount of empty results.
+    Returns:
+        zip_list: ([str, str, int]), contains file heading, list of dtypes
+        for heading, amount of empty results.
         summary_results ([str]): summary results of data.
         summary_indexes ([str]): summary headings for data.
     """
@@ -157,19 +175,24 @@ def get_column_information(df_file, dtypes_dict):
         validation_results.append(df_file[heading].isnull().sum())
         data_str = []
         for types in dtypes_dict[heading]:
-            data_str.append("" + str(types[1]) + " of data a " + str(types[0]) + " value.")
+            data_str.append("" +
+                            str(types[1]) +
+                            " of data a " +
+                            str(types[0]) +
+                            " value.")
         dtypes_list.append(data_str)
         column_detail = df_file[heading].describe()
         summary_results.append(np.array(column_detail).astype('str'))
         summary_indexes.append(list(column_detail.index))
 
-    #Get datapoint headings
+    # Get datapoint headings
     for field in Datapoints._meta.fields:
-        data_model_headings.append(field.name)#.get_attname_column())
-    #skip first four headings as irrelevant to user input, should use filter for this
+        data_model_headings.append(field.name)  # .get_attname_column())
+    # skip first four headings as irrelevant to user input, should use filter
+    # for this
 
     data_model_headings = DATAMODEL_HEADINGS
-    remaining_mapping = data_model_headings    
+    remaining_mapping = data_model_headings
 
     zip_list = zip(file_heading_list, dtypes_list, validation_results)
     return zip_list, summary_results, summary_indexes, remaining_mapping
--- ZOOM/validate/validator.py
+++ ZOOM/validate/validator.py
@@ -3,35 +3,37 @@ from lib.tools import identify_col_dtype
 from metadata.models import File
 from lib.common import get_geolocation_dictionary, save_validation_data, get_column_information
 
+
 def validate(id):
     """Perform validation check on file.
-    
+
     Args:
         id (int): ID of file.
-        
-    Returns: 
+
+    Returns:
         context ({str:data}): information related to the validation of file.
     """
-    
+
     print("Begining Validation")
-    
-    newdoc = [''] ### Future: using array in case user uploads multiple files
-    found_mapping = [] ### Future: automatic mapping ### 
+
+    newdoc = ['']  # Future: using array in case user uploads multiple files
+    found_mapping = []  # Future: automatic mapping ###
 
     newdoc[0] = str(File.objects.get(id=id).file)
 
-    ### Future: loop here if uploaded multiple files or distrubute with tasks
+    # Future: loop here if uploaded multiple files or distrubute with tasks
 
     df_file = pd.read_csv(newdoc[0])
     error_data, dtypes_dict = generate_error_data(df_file)
-    zip_list, summary_results, summary_indexes, remaining_mapping = get_column_information(df_file, dtypes_dict)
+    zip_list, summary_results, summary_indexes, remaining_mapping = \
+        get_column_information(df_file, dtypes_dict)
     print("Saving Error Information")
     save_validation_data(error_data, id, dtypes_dict)
     context = {
-        'success': 1, 
-        "found_list": zip_list, 
-        "summary":zip(summary_indexes, summary_results),
-        "missing_list" : remaining_mapping
+        'success': 1,
+        "found_list": zip_list,
+        "summary": zip(summary_indexes, summary_results),
+        "missing_list": remaining_mapping
     }
 
     return context
@@ -39,11 +41,11 @@ def validate(id):
 
 def generate_error_data(df_file):
     """Get error data for file.
-    
+
     Args:
         df_file (Dataframe): data of csv file in a dataframe.
 
-    Returns: 
+    Returns:
         error_data ({str:[str]}): error data for each column.
         dtypes_dict ({str:str}): stores the data-types for each heading.
     """
@@ -54,7 +56,8 @@ def generate_error_data(df_file):
     dicts = get_geolocation_dictionary()
 
     for heading in file_heading_list:
-        prob_list, error_count = identify_col_dtype(df_file[heading], heading, dicts)
+        prob_list, error_count = identify_col_dtype(
+            df_file[heading], heading, dicts)
         dtypes_dict[heading] = prob_list
         error_data[heading] = error_count
 
