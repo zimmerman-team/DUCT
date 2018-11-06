15017f990869ef4055b694a340710d6e2b76ec37
Kieran <kieran@machine-macmini.home>
2018-11-06 14:10:02 +0100

    Changed Mapping to be foreign key in file model

--- ZOOM/mapping/models.py
+++ ZOOM/mapping/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
-from metadata.models import File
-
-
 class Mapping(models.Model):
-    file = models.ForeignKey(File, on_delete=models.CASCADE)
-    data = JSONField()
+    data = JSONField(unique=True)
---
+++ ZOOM/scripts/migrate_order.sh
@@ -0,0 +1,5 @@
+python ../manage.py makemigrations mapping
+python ../manage.py makemigrations geodata
+python ../manage.py makemigrations metadata
+python ../manage.py makemigrations indicator
+python ../manage.py migrate
