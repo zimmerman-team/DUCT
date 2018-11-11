13d1eb3a33a7f77eb74ce4d8843a334717547a19
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-11-11 23:05:07 +0100

    Added entry id

--- ZOOM/gql/mapping/serializers.py
+++ ZOOM/gql/mapping/serializers.py
@@ -4,11 +4,17 @@ from mapping.models import Mapping
 
 
 class MappingSerializer(serializers.ModelSerializer):
+    entry_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Mapping
         fields = (
             'id',
             'file',
-            'data'
+            'data',
+            'entry_id'
         )
+
+    @classmethod
+    def get_entry_id(cls, obj):
+        return str(obj.id)
