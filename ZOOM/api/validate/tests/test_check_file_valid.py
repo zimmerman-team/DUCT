d6bc5aab2c1982894bc11699691e2a62a24e0c5d
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-02-05 12:24:24 +0100

    Fixed error the Rest API url & reformat PEP8

--- ZOOM/api/validate/tests/test_check_file_valid.py
+++ ZOOM/api/validate/tests/test_check_file_valid.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
-from unittest import skip
-from django.test import RequestFactory, Client
+from django.test import RequestFactory, TestCase
 from rest_framework.test import APIClient
+
+from geodata.importer.country import CountryImport
 
 
 class FileValidTestCase(TestCase):
@@ -9,54 +9,83 @@ class FileValidTestCase(TestCase):
     c = APIClient()
 
     def test_check_file_valid(self):
+        ci = CountryImport()
+        ci.update_polygon()
+        ci.update_alt_name()
 
-        '''
-        Test 1: Upload File
-        '''
+        """
+        Test 0: Add source
+        """
+        res = self.c.post(
+            '/api/metadata/sources/?format=json',
+            {
+                'name': 'The one',
+            })
 
+        self.assertEquals(res.status_code, 201, res.json())
+        self.assertIsNotNone(res.json()['id'])
+        id = res.json()['id']
+
+        """
+        Test 1: Upload file
+        """
         with open('samples/check_file_valid_test_success.csv') as fp:
-                res_file_upload = self.c.post(
-                        '/api/file/?format=json', 
-                        {
-                        'file': fp,
-                        'description': 'temp description',
-                        'title': 'check_file_valid_test_success.csv',
-                        })
+            res_file_upload = self.c.post(
+                '/api/metadata/?format=json',
+                {
+                    'file': fp,
+                    'title': 'temp title',
+                    'description': 'temp description',
+                    'contains_subnational_data': True,
+                    'organisation': 'ZZ',
+                    'maintainer': 'tester',
+                    'date_of_dataset': '2009-08-06',
+                    'methodology': 'Testing',
+                    'define_methodology': 'Really testing',
+                    'update_frequency': 'All the time',
+                    'comments': 'Good stuff',
+                    'accessibility': 'p',
+                    'data_quality': 'good',
+                    'number_of_rows': 200,
+                    'file_types': 'csv',
+                    'location': 1,
+                    'source': id,
+                })
 
-        self.assertEquals(res_file_upload.status_code, 201, res_file_upload.json())
+        self.assertEquals(res_file_upload.status_code, 201,
+                          res_file_upload.json())
         self.assertIsNotNone(res_file_upload.json()['id'])
 
         '''
-        Test 2: Check File is Valid
+        Test 3: Check File is Valid
         '''
 
         res_check_file_valid = self.c.post(
-            '/api/validate/check_file_valid/?format=json', 
+            '/api/validate/check_file_valid/?format=json',
             {
-            'id': res_file_upload.json()['id'],
+                'id': res_file_upload.json()['id'],
             },
             format='json'
-            )
+        )
 
-        self.assertEquals(res_check_file_valid.status_code, 200, res_check_file_valid.json())
+        self.assertEquals(res_check_file_valid.status_code, 200,
+                          res_check_file_valid.json())
         self.assertEquals(res_check_file_valid.json()['success'], 1)
 
         '''
-        Test 3: Check File Validation results
+        Test 4: Check File Validation results
         '''
 
         res_file_validate = self.c.post(
             '/api/validate/?format=json',
             {
-            'id': res_file_upload.json()['id'],
+                'id': res_file_upload.json()['id'],
             },
             format='json'
-            )
-        
-        self.assertEquals(res_file_validate.status_code, 200, res_file_validate.json())
+        )
+
+        self.assertEquals(res_file_validate.status_code, 200,
+                          res_file_validate.json())
         self.assertIsNotNone(res_file_validate.json()['found_list'])
         self.assertIsNotNone(res_file_validate.json()['missing_list'])
         self.assertIsNotNone(res_file_validate.json()['summary'])
-        
-        
-
