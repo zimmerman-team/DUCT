5604255d19c6e8da0a4f744af414c497d1f49b27
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-05-29 09:01:44 +0200

    Added PC2 and long lat for all post code NL

--- ZOOM/geodata/importer/post_code_nl.py
+++ ZOOM/geodata/importer/post_code_nl.py
@@ -1,4 +1,5 @@
 import json
+from django.contrib.gis.geos import fromstr
 
 from geodata.importer.common import get_json_data
 from geodata.models import PostCode, Country, Geolocation
@@ -106,7 +107,36 @@ class PostCodeNL(object):
 
             print(code)
 
+    def _updated_pc_centers(self, file):
+        centers = get_json_data(file)
+        for center in centers:
+            try:
+                pc = PostCode.objects.get(code=center.lower())
+                point_string = ''.join([
+                    'POINT(',
+                    str(centers[center]["longitude"]),
+                    ' ',
+                    str(centers[center]["latitude"]),
+                    ')'])
+                long_lat = fromstr(point_string, srid=4326)
+                pc.center_longlat = long_lat
+                try:
+                    geolocation = Geolocation.objects.get(tag=pc.code)
+                    geolocation.save()
+                    print("Updated {code}:".format(code=pc.code))
+                except Geolocation.DoesNotExist:
+                    pass
+            except PostCode.DoesNotExist:
+                print("Not found {code}:".format(code=center.lower()))
+
     def update(self):
+        self.update_pc2()
         self.update_pc4()
         self.update_pc6()
         self.update_pc3()
+
+    def update_centers(self):
+        self._updated_pc_centers('/../data_backup/PC2_centers.json')
+        self._updated_pc_centers('/../data_backup/PC3_centers.json')
+        self._updated_pc_centers('/../data_backup/PC4_centers.json')
+        self._updated_pc_centers('/../data_backup/PC6_centers.json')
