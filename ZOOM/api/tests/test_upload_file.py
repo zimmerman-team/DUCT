63bb1efad6eb7d9f33692c66e08627a81e6d0274
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-11-26 09:59:39 +0100

    Updated to delete file after test upload

--- ZOOM/api/tests/test_upload_file.py
+++ ZOOM/api/tests/test_upload_file.py
@@ -11,10 +11,23 @@ class UploadFileTestCase(TestCase):
     So please put it in the local_settings.py
     """
 
+    def setUp(self):
+        self.filename = 'AIDSinfotest'
+
     def test_file_upload(self):
         settings.UNIT_TEST = True
         client = Client()
-        with open(os.path.abspath("samples/AIDSinfotest.csv")) as fp:
+
+        ext = '.csv'
+        with open(os.path.abspath(
+            'samples/{filename}'.format(filename=self.filename + ext))
+        ) as fp:
             response = client.post('/api/metadata/upload/', {'file': [fp]})
 
         self.assertIsNotNone(response.data.get('url'))
+
+    def tearDown(self):
+        media_dir = settings.MEDIA_ROOT + '/datasets/'
+        for item in os.listdir(media_dir):
+            if item.startswith(self.filename):
+                os.remove(os.path.join(media_dir, item))
